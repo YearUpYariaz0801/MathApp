3d48a06baf3f69d6cba79c1706b35acaf2d32cb5
Yariaz0801 <designsbyzairay@gmail.com>
2024-10-31 08:21:44 -0400

    MathApp Progress Never Pushed

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,87 @@
+package com.pluralsight;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        int bobSalary = 250_000;
+        int garySalary = 342_000;
+
+        // then code solution
+        int highestSalary = Math.max(bobSalary,garySalary);
+
+        // then use System.out.println() to display results
+        System.out.println("The answer is " + highestSalary);
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 2:
+        // declare variables here
+        int carPrice = 46_255;
+        int truckPrice = 78_942;
+
+        // then code solution
+        int lowestCost = Math.min(carPrice,truckPrice);
+
+        // then use System.out.println() to display results
+        System.out.println("The answer is " + lowestCost);
+
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 3:
+        // declare variables here
+        double radius = 7.25;
+
+        // then code solution
+        //area = pi * radius ^2
+        double area = Math.PI * Math.pow(radius,2);
+        //double area = Math.PI(Math.pow(double radius));
+
+        // then use System.out.println() to display results
+        //System.out.println("The answer is " double area);
+        System.out.println(area);
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 4:
+        // declare variables here
+        double
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 5:
+        // declare variables here
+        int x1 = 5;
+        int x2 = 85;
+        int y1 = 10;
+        int y2 = 50;
+
+        // then code solution
+        //double distanceBetween = Math.sqrt(Math.pow((x2 - x1,2) + Math.pow(y2 -y1,2));
+
+        // then use System.out.println() to display results
+        //System.out.println("The distance between the two points is " + distanceBetween);
+
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 6:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 7:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+    }
+
+}
